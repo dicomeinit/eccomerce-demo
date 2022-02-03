8bca99659ed4d85bba31c18ba212102994b5c540
Diana <dicomeinit@gmail.com>
2022-02-03 22:57:37 +0200

    add link to cart and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import { Container } from "react-bootstrap";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return (
-    <div>
-        <Header/>
+    return (
+        <Router>
+            <Header />
             <main className="py-3">
                 <Container>
-                    <HomeScreen/>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} />
+                        <Route path="/product/:id" element={<ProductScreen />} />
+                    </Routes>
                 </Container>
             </main>
-        <Footer/>
-    </div>
+            <Footer />
+        </Router>
   );
 }
 
