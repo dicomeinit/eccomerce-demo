258ddc1c45e4e1497a5475d4342be45704a577fd
Diana <dicomeinit@gmail.com>
2022-03-09 23:43:22 +0200

    add loader

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector} from "react-redux";
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Loader from '../components/Loader';
 import {listProducts} from "../actions/productActions";
 
 function HomeScreen() {
@@ -17,7 +18,7 @@ function HomeScreen() {
     return (
         <div>
             <h1>Latest Products</h1>
-            {loading ? <h2>Loading...</h2>
+            {loading ? <Loader />
                 : error ? <h3>{error}</h3>
                     :
                     <Row>
