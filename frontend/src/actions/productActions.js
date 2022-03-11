eeb5e18e1a15bd02a3d400dd4b9a15f7816b5bde
Diana <dicomeinit@gmail.com>
2022-03-11 22:45:12 +0200

    add product details

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL
 } from "../constants/productConstants";
+import {useParams} from "react-router-dom";
 
 export const listProducts = (keyword = '') => async (dispatch) => {
     try {
@@ -20,6 +21,27 @@ export const listProducts = (keyword = '') => async (dispatch) => {
             type: PRODUCT_LIST_FAIL,
             payload: error.response && error.response.data.message
                 ? error.response.data.message
+                : error.message,
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+        const { data } = await axios.get(`http://127.0.0.1:8000/api/products/${id}/`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
                 : error.message,
         })
     }
