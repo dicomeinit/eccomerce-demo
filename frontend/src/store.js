3f34cae7fe3599d6f3b8ce044a09bfc09a82e182
Diana <dicomeinit@gmail.com>
2022-03-15 13:34:25 +0200

    add cartAction.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,12 @@ const reducer = combineReducers({
     cart: cartReducers,
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+        JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart:{ cartItemsFromStorage}
+}
 
 const middleware = [thunk]
 
