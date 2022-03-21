8383d3e2a2a7378eff90889bcbac8d4c895f1dc0
Diana <dicomeinit@gmail.com>
2022-03-21 12:54:14 +0200

    add trash icon and update css

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -14,9 +14,9 @@ import {
   Image,
   Form,
   Button,
-  Card,
+  Card, ListGroup,
 } from "react-bootstrap";
-import { Message } from "../components/Message";
+import Message from "../components/Message";
 import { addToCart, removeFromCart } from "../actions/cartAction";
 
 function CartScreen() {
@@ -41,10 +41,74 @@ function CartScreen() {
   }, [dispatch, id, qty]);
 
   const removeFromCartHandler = (id) => {
+    console.log('remove:', id)
     dispatch(removeFromCart(id))
   }
 
-  return <div>Cart</div>;
+  return (
+      <Row>
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          {cartItems.length === 0 ? (
+              <Message variant='info'>
+                Your cart is empty <Link to='/'>Go Back</Link>
+              </Message>
+          ) : (
+              <ListGroup variant='flush'>
+                {cartItems.map(item => (
+                    <ListGroup.Item key={item.product}>
+                      <Row>
+                        <Col md={2}>
+                          <Image src={item.image} alt={item.name} fluid rounded/>
+                        </Col>
+
+                        <Col md={3}>
+                          <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        </Col>
+
+                        <Col md={2}>
+                          ${item.price}
+                        </Col>
+
+                        <Col md={3}>
+                          <Form.Control
+                          as="select"
+                          value={item.qty}
+                          onChange={(e) => dispatch(addToCart(item.product, e.target.value))}
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+
+                        </Col>
+
+                        <Col md={1}>
+                          <Button
+                            type='button'
+                            variant='light'
+                          onClick={() => removeFromCartHandler(item.product)}>
+                              <i className='fas fa-trash'></i>
+                          </Button>
+
+                        </Col>
+                      </Row>
+
+                    </ListGroup.Item>
+                ))}
+              </ListGroup>
+          )}
+
+
+        </Col>
+
+        <Col md={4}>
+
+        </Col>
+      </Row>
+  )
 }
 
 export default CartScreen;
