24fd3b45097f4c58ab94c13d44b503597fc3882e
Diana <dicomeinit@gmail.com>
2022-05-04 01:33:45 +0300

    fix user views

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -51,21 +51,6 @@ def registerUser(request):
 
 
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        'api/products/',
-        'api/products/create/',
-        'api/products/upload/',
-        'api/products/<id>/reviews/',
-        'api/products/top/',
-        'api/products/<id>/',
-        'api/products/delete/<id>/',
-        'api/products/<update>/<id>/',
-    ]
-    return Response(routes)
-
-
-@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
