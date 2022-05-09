6690c9c5c685d941935d97777f3182be5abd5b14
Diana <dicomeinit@gmail.com>
2022-05-10 01:29:10 +0300

    add possibility to update profile data

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -12,7 +12,12 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
-  USER_DETAILS_RESET
+  USER_DETAILS_RESET,
+
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_RESET,
 
 } from "../constants/userConstants";
 
@@ -50,18 +55,41 @@ export const userRegisterReducers = (state = {}, action) => {
 };
 
 
-export const userDetailsReducers = (state = {user:{}}, action) => {
-  switch (action.type) {
-    case USER_DETAILS_REQUEST:
-      return {...state, loading: true };
-    case USER_DETAILS_SUCCESS:
-      return { loading: false, user: action.payload };
-    case USER_DETAILS_FAIL:
-      return { loading: false, error: action.error };
-    case USER_DETAILS_RESET:
-      return { user: {}};
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
 
-    default:
-      return state
-  }
-};
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_DETAILS_RESET:
+            return { user: {} }
+
+
+        default:
+            return state
+    }
+}
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true }
+
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload }
+
+        case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
+
+        default:
+            return state
+    }
+}
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import {Link, useLocation, useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import {getUserDetails} from '../actions/userActions'
+import {getUserDetails, updateUserProfile} from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 function ProfileScreen() {
 
@@ -24,18 +25,21 @@ function ProfileScreen() {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
+
     useEffect(() => {
         if (!userInfo) {
             navigate('/login')
         }else{
-            if(!user || !user.name){
-                dispatch(getUserDetails('profile'))
+            if(!user || !user.name || success){
+                dispatch({type:USER_UPDATE_PROFILE_RESET})
             }else{
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [dispatch, navigate, userInfo, user])
+    }, [dispatch, navigate, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -43,7 +47,12 @@ function ProfileScreen() {
         if (password !== confirmPassword) {
             setMessage('Passwords do not match')
         } else {
-            console.log('Updating..')
+            dispatch(updateUserProfile({
+                'id':user._id,
+                'name':name,
+                'email':email,
+                'password':password
+            }))
         }
 
     }
