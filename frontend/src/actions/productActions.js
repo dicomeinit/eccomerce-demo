9e3a7624d2ee4090c34f7ac3b825a5111b973671
Diana <68649190+dicomeinit@users.noreply.github.com>
2022-05-10 20:36:24 +0300

    Add prettier config (#3)

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,56 +1,58 @@
-import axios from "axios";
+import axios from 'axios';
 import {
-  PRODUCT_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL,
-  PRODUCT_DETAILS_REQUEST,
-  PRODUCT_DETAILS_SUCCESS,
-  PRODUCT_DETAILS_FAIL,
-} from "../constants/productConstants";
-import { useParams } from "react-router-dom";
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+} from '../constants/productConstants';
+import { useParams } from 'react-router-dom';
 
 export const listProducts =
-  (keyword = "") =>
-  async (dispatch) => {
-    try {
-      dispatch({ type: PRODUCT_LIST_REQUEST });
+    (keyword = '') =>
+    async (dispatch) => {
+        try {
+            dispatch({ type: PRODUCT_LIST_REQUEST });
 
-      const { data } = await axios.get("http://127.0.0.1:8000/api/products/");
+            const { data } = await axios.get(
+                'http://127.0.0.1:8000/api/products/'
+            );
 
-      dispatch({
-        type: PRODUCT_LIST_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_LIST_FAIL,
-        payload:
-          error.response && error.response.data.detail
-            ? error.response.data.detail
-            : error.message,
-      });
-    }
-  };
+            dispatch({
+                type: PRODUCT_LIST_SUCCESS,
+                payload: data
+            });
+        } catch (error) {
+            dispatch({
+                type: PRODUCT_LIST_FAIL,
+                payload:
+                    error.response && error.response.data.detail
+                        ? error.response.data.detail
+                        : error.message
+            });
+        }
+    };
 
 export const listProductDetails = (id) => async (dispatch) => {
-  try {
-    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(
-      `http://127.0.0.1:8000/api/products/${id}/`
-    );
+        const { data } = await axios.get(
+            `http://127.0.0.1:8000/api/products/${id}/`
+        );
 
-    dispatch({
-      type: PRODUCT_DETAILS_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: PRODUCT_DETAILS_FAIL,
-      payload:
-        error.response && error.response.data.detail
-          ? error.response.data.detail
-          : error.message,
-    });
-  }
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        });
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload:
+                error.response && error.response.data.detail
+                    ? error.response.data.detail
+                    : error.message
+        });
+    }
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,51 +1,51 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import {
-  productListReducers,
-  productDetailsReducers,
-} from "./reducers/productReducers";
-import { cartReducers } from "./reducers/cartReducers";
+    productListReducers,
+    productDetailsReducers
+} from './reducers/productReducers';
+import { cartReducers } from './reducers/cartReducers';
 import {
-  userLoginReducers,
-  userRegisterReducers,
-  userDetailsReducer,
-  userUpdateProfileReducer,
-} from "./reducers/userReducers";
+    userLoginReducers,
+    userRegisterReducers,
+    userDetailsReducer,
+    userUpdateProfileReducer
+} from './reducers/userReducers';
 
 const reducer = combineReducers({
-  productList: productListReducers,
-  productDetails: productDetailsReducers,
-  cart: cartReducers,
-  userLogin: userLoginReducers,
-  userRegister: userRegisterReducers,
-  userDetails: userDetailsReducer,
-  userUpdateProfile: userUpdateProfileReducer,
+    productList: productListReducers,
+    productDetails: productDetailsReducers,
+    cart: cartReducers,
+    userLogin: userLoginReducers,
+    userRegister: userRegisterReducers,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 });
 
-const cartItemsFromStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [];
 
-const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null;
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null;
 
-const shippingAddress = localStorage.getItem("shippingAddress")
-  ? JSON.parse(localStorage.getItem("shippingAddress"))
-  : {};
+const shippingAddress = localStorage.getItem('shippingAddress')
+    ? JSON.parse(localStorage.getItem('shippingAddress'))
+    : {};
 
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage, shippingAddress },
-  userLogin: { userInfo: userInfoFromStorage },
+    cart: { cartItems: cartItemsFromStorage, shippingAddress },
+    userLogin: { userInfo: userInfoFromStorage }
 };
 
 const middleware = [thunk];
 
 const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
