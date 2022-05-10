10cdb69b5ea785e90a98b50c6eaa42ffda0a4386
Diana <dicomeinit@gmail.com>
2022-05-10 20:36:02 +0300

    Add prettier config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,44 @@
-import { Container } from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import ShippingScreen from "./screens/ShippingScreen";
-import PaymentScreen from "./screens/PaymentScreen";
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/register" element={<RegisterScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-            <Route path="/shipping" element={<ShippingScreen />} />
-            <Route path="/payment" element={<PaymentScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="cart" element={<CartScreen />}>
-              <Route path="/cart/:id" element={<CartScreen />} />
-            </Route>
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} />
+                        <Route path="/login" element={<LoginScreen />} />
+                        <Route path="/register" element={<RegisterScreen />} />
+                        <Route path="/profile" element={<ProfileScreen />} />
+                        <Route path="/shipping" element={<ShippingScreen />} />
+                        <Route path="/payment" element={<PaymentScreen />} />
+                        <Route
+                            path="/product/:id"
+                            element={<ProductScreen />}
+                        />
+                        <Route path="cart" element={<CartScreen />}>
+                            <Route path="/cart/:id" element={<CartScreen />} />
+                        </Route>
+                    </Routes>
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
