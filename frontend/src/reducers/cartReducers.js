5868239089fbbdc25f0e47cf1095c90c866e83c1
Diana <68649190+dicomeinit@users.noreply.github.com>
2022-05-12 21:19:13 +0300

    8 create order (#10)
    * create constants for order
    
    * add OrderReducer
    
    * implementation order action
    
    * fix backend
    
    * update action, reducer, constants for order
    
    * update action, reducer, constants for cart
    
    * add PlaceOrderScreen
    
    * add format

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,7 +2,8 @@ import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
-    CART_SAVE_PAYMENT_METHOD
+    CART_SAVE_PAYMENT_METHOD,
+    CART_CLEAR_ITEMS
 } from '../constants/cartConstants';
 
 export const cartReducers = (
@@ -48,6 +49,12 @@ export const cartReducers = (
             return {
                 ...state,
                 paymentMethod: action.payload
+            };
+
+        case CART_CLEAR_ITEMS:
+            return {
+                ...state,
+                cartItems: []
             };
         default:
             return state;
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -8,12 +8,20 @@ import {
     Card,
     ListGroupItem
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { createOrder } from '../actions/orderAction';
+import { ORDER_CREATE_RESET } from '../constants/OrderConstants';
 
 function PlaceOrderScreen() {
+    let navigate = useNavigate();
+
+    const orderCreate = useSelector((state) => state.orderCreate);
+    const { order, error, success } = orderCreate;
+
+    const dispatch = useDispatch();
     const cart = useSelector((state) => state.cart);
 
     cart.itemsPrice = cart.cartItems
@@ -28,7 +36,29 @@ function PlaceOrderScreen() {
         Number(cart.taxPrice)
     ).toFixed(2);
 
+    if (!cart.paymentMethod) {
+        navigate('/payment');
+    }
+
+    useEffect(() => {
+        if (success) {
+            navigate(`/order/${order._id}`);
+            dispatch({ type: ORDER_CREATE_RESET });
+        }
+    }, [success, navigate]);
+
     const placeOrder = () => {
+        dispatch(
+            createOrder({
+                orderItems: cart.cartItems,
+                shippingAddress: cart.shippingAddress,
+                paymentMethod: cart.paymentMethod,
+                itemsPrice: cart.itemsPrice,
+                shippingPrice: cart.shippingPrice,
+                taxPrice: cart.taxPrice,
+                totalPrice: cart.totalPrice
+            })
+        );
         console.log('Place order');
     };
     return (
@@ -140,6 +170,12 @@ function PlaceOrderScreen() {
                                     <Col>Total:</Col>
                                     <Col>${cart.totalPrice}</Col>
                                 </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                {error && (
+                                    <Message variant="danger">{error}</Message>
+                                )}
                             </ListGroup.Item>
 
                             <ListGroup>
