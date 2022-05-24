6a9c7d1f5ad6acfb6afc757249cf5a70357619d8
Diana <68649190+dicomeinit@users.noreply.github.com>
2022-05-24 23:10:03 +0300

    add functionality for creating product by admin (#44)

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,8 +4,9 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { listProducts, deleteProduct } from "../actions/productActions";
+import { listProducts, deleteProduct, createProduct } from "../actions/productActions";
 import {useNavigate} from "react-router-dom";
+import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
 
 function ProductListScreen() {
 
@@ -19,18 +20,27 @@ function ProductListScreen() {
     const productDelete = useSelector(state => state.productDelete);
     const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete;
 
+    const productCreate = useSelector(state => state.productCreate);
+    const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate;
+
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
 
     useEffect(() => {
-        if (userInfo && userInfo.isAdmin) {
-            dispatch(listProducts())
-        } else {
-            navigate('/login')
+        dispatch({ type: PRODUCT_CREATE_RESET })
+
+        if (!userInfo.isAdmin) {
+            navigate(`/login`)
         }
 
-    }, [dispatch, navigate, userInfo, successDelete])
+        if (successCreate) {
+            navigate(`/admin/product/${createdProduct._id}/edit`)
+        } else {
+            dispatch(listProducts())
+        }
+
+    }, [dispatch, navigate, userInfo, successDelete, successCreate, createdProduct])
 
     const deleteHandler = (id) => {
 
@@ -39,8 +49,8 @@ function ProductListScreen() {
         }
     }
 
-    const createProductHandler = (product) => {
-        //Create product
+    const createProductHandler = () => {
+        dispatch(createProduct())
     }
 
     return (
@@ -58,6 +68,10 @@ function ProductListScreen() {
 
             {loadingDelete && <Loader/>}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message> }
+
+            {loadingCreate && <Loader/>}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message> }
+
             {loading
                 ? <Loader/>
                 : error
