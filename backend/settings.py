885b39638e0c0484d72a249b48f16735307c3589
Diana <dicomeinit@gmail.com>
2022-06-23 14:53:23 +0300

    update staticfiles setting

--- backend/settings.py
+++ backend/settings.py
@@ -164,15 +164,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
-#     BASE_DIR / 'frontend/build/static'
+#     BASE_DIR / 'frontend' / 'build'/' static'
 # ]
 STATICFILES_DIRS = (
-    os.path.join(os.path.join(BASE_DIR, 'frontend'), 'build', 'static'),
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 )
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
@@ -198,6 +198,14 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# s3 static settings
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 if os.getcwd() == '/app':
     DEBUG = False
